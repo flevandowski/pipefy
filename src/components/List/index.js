4b14f929f78db8e1e3ae0f6a3e37b4b28b205fcd
Fernando Levandoski <fernando.levandowski@hotmail.com>
2019-08-19 17:02:22 -0300

    Corrigindo Drag and Drop

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -13,10 +13,10 @@ export default function List({ data, index: listIndex }) {
         <h2>{data.title}</h2>
         {data.creatable && (
           <button type="button">
-            <MdAdd size={24} color="#FFF" />
+            <MdAdd size={24} color="#FFF" /> 
           </button>
         )}
-      </header>
+      </header>      
 
       <ul>
         { data.cards.map((card, index) => (
